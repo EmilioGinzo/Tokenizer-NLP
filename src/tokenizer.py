d80b4ee3bc64c4598562cd36c2d60ba85adeee0b
Emilio Ginzo <68378278+EmilioGinzo@users.noreply.github.com>
2024-06-18 22:58:36 +0000

    initial commit

---
+++ src/tokenizer.py
@@ -0,0 +1,10 @@
+#leer un archivo de entrada .txt
+
+
+# Preprocesamiento del texto : dividir el texto en lexemas
+
+# Identificacion de tokens: utiliza expresiones regulares para identificar y clasificar los lexemas
+
+# Actualización del diccionario de datos: Añade los lexemas y sus posiciones al diccionario de datos.
+
+# Manejo de errores léxicos: Pregunta al usuario cómo clasificar lexemas no reconocidos.
